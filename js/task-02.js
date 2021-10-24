98ef1641475eeca03052a2442195400bd954365f
vik_orel <vikorel34@gmail.com>
2021-10-24 17:53:41 +0300

    completed 2nd task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listTitle = document.querySelector("ul");
+const list = function itemsCreator(arr) {
+  arr.forEach((element) => {
+    const createItem = document.createElement("li");
+    const addTextContent = (createItem.textContent = element);
+    const addClass = createItem.classList.add("item");
+    const pushItem = listTitle.append(createItem);
+  });
+};
+console.log(list(ingredients));
