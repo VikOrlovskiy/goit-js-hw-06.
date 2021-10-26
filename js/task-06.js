2c4ad3ee6c05532be87e1d70f445eef3003c09a5
vik_orel <vikorel34@gmail.com>
2021-10-26 18:19:03 +0300

    aded changes at task № 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,12 +2,11 @@ const refs = {
   inputRef: document.querySelector("#validation-input"),
 };
 refs.inputRef.addEventListener("blur", (event) => {
-  const inputText = event.currentTarget.value;
-  if (inputText.length === 6) {
-    console.log("ok");
+  let inputText = event.currentTarget.value;
+
+  if (inputText.length === +refs.inputRef.dataset.length) {
+    refs.inputRef.classList.remove("invalid");
     return refs.inputRef.classList.add("valid");
   }
   return refs.inputRef.classList.add("invalid");
 });
-console.log(refs.inputRef);
-console.log(refs.inputRef);
