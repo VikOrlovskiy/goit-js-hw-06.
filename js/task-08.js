fac2c43d150299391a65d4f1d3bc811ced1d849d
vik_orel <vikorel34@gmail.com>
2021-10-29 14:37:19 +0300

    8 task completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (onEmptyString(email) || onEmptyString(password)) {
+    return alert("все поля должны быть заполнены");
+  }
+  const formData = {
+    email,
+    password,
+  };
+  formRef.reset();
+  console.log(formData);
+}
+
+function onEmptyString(str) {
+  return !str || 0 === str.length;
+}
