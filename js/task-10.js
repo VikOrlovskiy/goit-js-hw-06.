6bba5c558ef6a25d486b17fb69a30856e208ebb4
vik_orel <vikorel34@gmail.com>
2021-10-29 17:43:55 +0300

    10  task done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
+const blockControls = document.querySelector("#controls");
+const inputField = blockControls.querySelector("input");
+const btnCreate = blockControls.querySelector("[data-create]");
+const btnDestroy = blockControls.querySelector("[data-destroy]");
+
+const motherBlock = document.querySelector("#boxes");
+motherBlock.style.display = `flex`;
+motherBlock.style.flexWrap = `wrap`;
+// ---------------------------------------------
+let BoxesCollection = [];
+let sizesOfBox = 30;
+// ---------------------------------------------
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// ---------------------------------------------
+function createBoxes(amount) {
+  console.log(amount);
+  for (var i = 0; i < amount; i += 1) {
+    let createEl = document.createElement("div");
+    createEl.style.width = `${sizesOfBox}px`;
+    createEl.style.height = `${sizesOfBox}px`;
+    sizesOfBox += 10;
+    createEl.style.backgroundColor = getRandomHexColor();
+    BoxesCollection.push(createEl);
+  }
+}
+// ---------------------------------------------
+function pushBoxesCollection() {
+  createBoxes(Number(inputField.value));
+  motherBlock.append(...BoxesCollection);
+}
+// ---------------------------------------------
+btnCreate.addEventListener("click", () => {
+  pushBoxesCollection();
+  inputField.value = 0;
+});
+btnDestroy.addEventListener("click", () => {
+  window.location.reload();
+});
