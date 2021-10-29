dca107609b90000be8ad053c0d114f19bdab72d4
vik_orel <vikorel34@gmail.com>
2021-10-29 15:38:38 +0300

    9 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,6 +1,7 @@
 const formRef = document.querySelector(".login-form");
 
 formRef.addEventListener("submit", onFormSubmit);
+
 function onFormSubmit(event) {
   event.preventDefault();
   const formElements = event.currentTarget.elements;
