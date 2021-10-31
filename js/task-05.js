76f3f9e1413f7fe9ddc84ece0f21b33943015090
vik_orel <vikorel34@gmail.com>
2021-10-31 10:45:48 +0200

    corrected mistake in 5 task

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,14 @@ const refs = {
   inputRef: document.querySelector("#name-input"),
   titleSpanRef: document.querySelector("#name-output"),
 };
+
 refs.inputRef.addEventListener("input", (event) => {
+  if (onEmptyString(event.currentTarget.value)) {
+    return (refs.titleSpanRef.textContent = "Anonymous");
+  }
   refs.titleSpanRef.textContent = event.currentTarget.value;
 });
+
+function onEmptyString(str) {
+  return !str || 0 === str.length;
+}
